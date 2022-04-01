4e71c329f729037364a29298771c6c7780cb50c7
Daffa Z <daffazakyy8@gmail.com>
2022-04-02 02:54:25 +0700

    again

--- src/handler.js
+++ src/handler.js
@@ -74,7 +74,7 @@ const updateNoteByIDHandler = (request, h) => {
     return h.response({
       status: 'success',
       message: 'Catatan berhasil diperbaharui',
-      data: noteFound,
+      data: { note: noteFound },
     });
   }
 
